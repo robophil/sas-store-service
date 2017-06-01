cd9064413a803de145903e628001de98367cec3a
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-01 10:25:12 +0100

    load all services in ./app directory

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //try to establish connection to the database
 const db = require('./db')
+const glob = require("glob")
+
+//config options for glob
+var options = {}
+
 db.start().then(models => {
     for (var key in models.collections) {
         var model = key.toLocaleLowerCase()
@@ -8,7 +13,29 @@ db.start().then(models => {
         //make models global
         global[model] = models.collections[key]
     }
+}).then(() => {
+    //load all service in ./app directory
+    loadServices().then(console.log).catch(console.log)
 }).catch(err => {
     console.log(err.message)
     console.log(err.error)
 })
+
+/**
+ * Function for loading services
+ */
+function loadServices() {
+    return new Promise((resolve, reject) => {
+        glob("app/*.js", options, function (err, files) {
+            if (err) return reject({ error: err, message: 'error loading services' })
+
+            files.map(file => "./" + file.substr(0, file.length - 3)).forEach(file => {
+                //load microservice
+                require(file)
+                console.log(`Service ${file} has been loaded`)
+            })
+
+            return resolve("All services has been loaded")
+        })
+    })
+}
